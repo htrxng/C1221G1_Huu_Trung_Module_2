76ddc59c504b91adcc417a811d95ca6fba63b24c
huutrungu <huutrungg02@gmail.com>
2022-02-17 17:15:18 +0700

    nop bai tap

---
+++ src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/find_biggest_element_in_two_way_array/FindBiggestElementInTwoWayArray.java
@@ -0,0 +1,12 @@
+package ss3_mang_va_phuong_thuc_trong_java.bai_tap.find_biggest_element_in_two_way_array;
+
+public class FindBiggestElementInTwoWayArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        
+    }
+}
