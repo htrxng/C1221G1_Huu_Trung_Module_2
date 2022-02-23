eb7eeed322ff8bebbbb7a9afbd3633ae87d610c5
huutrungu <huutrungg02@gmail.com>
2022-02-23 22:38:30 +0700

    nop bt

---
+++ src/ss6_inheritance/bai_tap/circle_cylinder_class/TestCylinder.java
@@ -0,0 +1,24 @@
+package ss6_inheritance.bai_tap.circle_cylinder_class;
+
+import java.util.Scanner;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Cylinder cylinder1 = new Cylinder();
+
+        cylinder1.setRadius(5);
+        cylinder1.areaCalculator();
+        cylinder1.setHeigth(5);
+        cylinder1.volumneCalculator();
+        System.out.println(cylinder1);
+
+
+        Circle circle1 = new Circle();
+
+        circle1.setRadius(7);
+        circle1.setColor("Grey");
+        circle1.areaCalculator();
+        System.out.println(circle1);
+    }
+
+}
---
+++ src/ss6_inheritance/bai_tap/point_2D_point_3D_class/Point2D.java
@@ -0,0 +1,51 @@
+package ss6_inheritance.bai_tap.point_2D_point_3D_class;
+
+import java.util.Arrays;
+
+public class Point2D {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+
+    public Point2D() {
+
+    }
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        return new float[]{x, y};
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
